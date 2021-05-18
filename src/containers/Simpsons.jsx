d20a7df23689a1ad7421838b5e15b74256b236f8
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-05-17 20:11:29 -0400

    setup container and test

---
+++ src/containers/Simpsons.jsx
@@ -0,0 +1,10 @@
+import React from 'react'; 
+
+const Simpsons = () => {
+    return(
+        <h1>Helloooooo</h1>
+    ); 
+} 
+
+
+export default Simpsons; 
---
+++ src/containers/simpsons.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react'; 
+import Simpsons from './Simpsons'; 
+
+
+describe('Simpsons Container', () => {
+    it('returns a quote on button click', () => {
+        render(<Simpsons />); 
+
+
+
+    })
+})
